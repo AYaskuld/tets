9af80b1685f4c4f2a4cfe0643dd2dde556c4b207
Artyom Yaskuld <98359811+AYaskuld@users.noreply.github.com>
2024-04-29 20:11:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello from Laravel!</title>
+</head>
+<body>
+    <h1>Hello from Laravel!</h1>
+    <p>This is a simple page created with Laravel.</p>
+</body>
+</html>
