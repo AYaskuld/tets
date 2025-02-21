8b5b4505ff0feec68ad4204657a314b2083e5013
kvothe <kvothe@ayagroup.pro>
2025-02-21 17:16:09 +0300

    added wsgi

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from src.app import create_app
-from src.config.server import Server_conf
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(Server_conf.SERVER_PORT))
+    app.run(debug=True)
