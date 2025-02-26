ca0ead57f9a3ee0f60680b9539be2f092c705dd7
kvothe <kvothe@ayagroup.pro>
2025-02-27 01:01:39 +0300

    table user renamed to users

---
+++ src/models/users.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from datetime import datetime
+from typing import Optional
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=False, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    deleted_at: Mapped[Optional[datetime]] = mapped_column(nullable=True)
